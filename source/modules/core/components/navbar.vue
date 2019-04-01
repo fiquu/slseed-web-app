3e39af6b3cb36adfdf476e8f1da42c021fd64e26
Santiago G. Marín <santiago@finaldevstudio.com>
2019-04-01 12:51:12 -0300

    Updated navbar behavior

--- source/modules/core/components/navbar.vue
+++ source/modules/core/components/navbar.vue
@@ -6,8 +6,8 @@ en:
 <template lang="pug">
 header.ui.fixed.top.compact.menu
   router-link.item(
-    v-if="$route.path !== '/' && $auth.isSignedIn()"
-    to='/'
+    v-if="$route.path !== '/dashboard' && $auth.isSignedIn()"
+    to='/dashboard'
     )
 
     i.chevron.left.icon
