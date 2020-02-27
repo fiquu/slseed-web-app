a06ed4380864410ef4131ea1daa17c41d83de093
Santiago G. Marín <santiago.marin@dotstudio.io>
2020-02-27 15:49:01 -0300

    Updated stack setup

--- setup/stack/template/index.js
+++ setup/stack/template/index.js
@@ -2,10 +2,12 @@ const { title } = require('../../../package.json');
 
 const Parameters = require('./parameters');
 const Resources = require('./resources');
+const Outputs = require('./outputs');
 
 module.exports = {
   Description: `${title} Base Stack [${process.env.NODE_ENV}]`,
   AWSTemplateFormatVersion: '2010-09-09',
   Parameters,
-  Resources
+  Resources,
+  Outputs
 };
