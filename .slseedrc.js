8ce092217c588c3de101f3e883bdbbcc08e67b51
Santiago G. Marín <santiago.marin@dotstudio.io>
2020-03-16 13:22:38 -0300

    Updated Slseed config

--- .slseedrc.js
+++ .slseedrc.js
@@ -4,7 +4,7 @@ const { join } = require('path');
 module.exports = {
   stack: name.replace(/\W+/g, ' ').trim().replace(/\s+/g, '-'),
   configs: join(__dirname, 'configs'),
+  package: require('./package.json'),
   dist: join(__dirname, 'dist'),
-  root: __dirname,
   type: 'app'
 }
