aa4b2b801c07d71759b888db8fa0bc1ac2dd7dc2
Santiago G. Marín <santiago.marin@final.cl>
2020-07-27 16:12:32 -0400

    Cleanup

--- configs/webpack.js
+++ configs/webpack.js
@@ -1,12 +1,9 @@
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const CopyPlugin = require('copy-webpack-plugin');
-const { IgnorePlugin } = require('webpack');
 
 module.exports = {
   devServer: require('./dev-server'),
   devtool: 'source-map',
   plugins: [
-    new IgnorePlugin(/^\.\/locale$/, /moment$/), // Don't load every Moment locale (many kbs!).
     new BundleAnalyzerPlugin({
       analyzerMode: process.env.BUNDLE_ANALYZER || 'disabled',
       openAnalyzer: false
